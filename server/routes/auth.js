3d5e0c1d9d648c83dd263f6c38a42e072c0ce76e
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-06 15:13:19 -0800

    Auth route

---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router(); 
+
+const {
+    signup
+} = require('../controllers/auth')
+
+router.get('/signup',signup);
+
+
+module.exports = router;
