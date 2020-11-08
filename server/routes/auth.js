e0b161995813eba593e6872f6fba823e3961a2d8
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-07 19:52:10 -0800

    Account-activation route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router(); 
 
 const {
-    signup
+    signup,
+    accountActivation
 } = require('../controllers/auth')
 
 const { userSignupValidator } = require('../validators/auth') 
 const { runValidation  } = require('../validators/index') 
 
 router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/account-activation', accountActivation);
 
 
 module.exports = router;
