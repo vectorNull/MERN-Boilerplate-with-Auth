9870f17176bc42a1886aa3d094054b113a287b6b
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2020-11-11 00:25:00 -0800

    Added color to links

--- client/src/core/Layout.jsx
+++ client/src/core/Layout.jsx
@@ -1,32 +1,49 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom'
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
 
-const Layout = ({ children }) => {
-    const nav = () => (
-        <ul className="nav nav-tabs bg-primary">
-            <li className="nav-item">
-                <Link to="/" className="text-light nav-link">
-                    Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/signup" className="text-light nav-link">
-                    Sign up
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/signin" className="text-light nav-link">
-                    Sign in
-                </Link>
-            </li>
-        </ul>
-    );
+const Layout = ({ children, match }) => {
+	const isActive = path => {
+		if (match.path === path) {
+			return { color: "#000" };
+		} else {
+			return { color: "#fff" };
+		}
+	};
 
-    return (
-        <Fragment>
-            {nav()}
-            <div className="container">{children}</div>
-        </Fragment>
-    );
+	const nav = () => (
+		<ul className='nav nav-tabs bg-primary'>
+			<li className='nav-item'>
+				<Link
+					to='/'
+					className='nav-link'
+					style={isActive("/")}>
+					Home
+				</Link>
+			</li>
+			<li className='nav-item'>
+				<Link
+					to='/signup'
+					className='nav-link'
+					style={isActive("/signup")}>
+					Sign up
+				</Link>
+			</li>
+			<li className='nav-item'>
+				<Link
+					to='/signin'
+					className='nav-link'
+					style={isActive("/signin")}>
+					Sign in
+				</Link>
+			</li>
+		</ul>
+	);
+
+	return (
+		<Fragment>
+			{nav()}
+			<div className='container'>{children}</div>
+		</Fragment>
+	);
 };
-export default Layout;
+export default withRouter(Layout);
